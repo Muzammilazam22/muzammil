7e5dfe85fa1fb6d5f7fefab0ebcc71098d62e256
muzammil <muzammil.azam88@gmail.com>
2021-05-11 15:39:40 +0500

    final & complete code

--- contracts/Rupees.sol
+++ contracts/Rupees.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Rupees {}
+contract Rupees is ERC20("Rupees", "RS") {
+    constructor() {
+        _mint(0x9C7F02485d5eb4b17BA6756985697bCC712061fa, 1000 * 1e18);
+    }
+}
